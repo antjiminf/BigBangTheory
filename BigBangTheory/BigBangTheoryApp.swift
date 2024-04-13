758d9e77a8b1f5286a5c91eb45e601de82b7dad2
Antonio Jiménez Infante <antoniojz1998@gmail.com>
2024-04-13 13:05:28 +0200

    Initial Commit

---
+++ BigBangTheory/BigBangTheoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBangTheoryApp.swift
+//  BigBangTheory
+//
+//  Created by Antonio Jiménez Infante on 13/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BigBangTheoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
